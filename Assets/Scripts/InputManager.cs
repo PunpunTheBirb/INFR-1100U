656aca80128be6d7324b5e62ce2911e785a281e2
PunpunTheBirb <145882381+PunpunTheBirb@users.noreply.github.com>
2023-10-25 17:27:04 -0700

    Movement added

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class InputManager 
+{
+    private static Controls _controls;
+
+    public static void init(Player myPlayer)
+    {
+        _controls = new Controls();
+
+        _controls.Game.Movement.performed += hi =>
+        {
+            myPlayer.SetMovementDirection(hi.ReadValue<Vector3>());
+        };
+
+        _controls.Permanent.Enable();
+
+    }
+
+
+    public static void GameMode()
+    {
+        _controls.Game.Enable();
+        _controls.UI.Disable();
+    }
+    public static void UIMode()
+    {
+        _controls.UI.Enable();
+        _controls.Game.Disable();
+    }
+}
